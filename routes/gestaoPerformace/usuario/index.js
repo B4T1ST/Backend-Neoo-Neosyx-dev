7376cf4aee90d8aeb6261d3a266bf8c8b8fe730c
B4T1ST <ds.batista1994@hotmail.com>
2024-02-29 18:38:06 -0300

    ADD - funcao de dados colaborador

--- routes/gestaoPerformace/usuario/index.js
+++ routes/gestaoPerformace/usuario/index.js
@@ -1,36 +1,57 @@
-//carregando modulos
+// Importando módulos
 const sql = require('mssql');
 const express = require('express');
 const router = express.Router();
 const config = require('../../../config/config.json');
-const { get } = require('../../../lib/poolManager')
+const { get } = require('../../../lib/poolManager');
 const connection = require('../../../config/' + config.banco);
 
-router.get('/', function (req, res) {
-    const {
-        almope
-    } = req.query
+// Função para transformar o formato do objeto
+function transformarFormato(objetoOriginal) {
+  const novoFormato = {
+    "cargo": []
+  };
 
-    retornaDadosUsuario(almope,  res)
-});
+  for (const chave in objetoOriginal) {
+    if (objetoOriginal.hasOwnProperty(chave)) {
+      const novoArray = [];
 
-async function retornaDadosUsuario(almope, res) {
+      for (const propriedade in objetoOriginal[chave]) {
+        if (objetoOriginal[chave].hasOwnProperty(propriedade)) {
+          const novoItem = {
+            label: propriedade,
+            value: objetoOriginal[chave][propriedade]
+          };
+
+          novoArray.push({ [propriedade]: novoItem });
+        }
+      }
+
+      novoFormato["cargo"].push(...novoArray);
+    }
+  }
+
+  return novoFormato;
+}
+
+// Rota
+router.get('/', async function (req, res) {
+    const { almope } = req.query;
+
     try {
-
-        let pool = await get('BDRechamadasGeral', connection)
+        let pool = await get('BDRechamadasGeral', connection);
         let resultColaborador = await pool.request()
             .input('almope', sql.VarChar, almope)
-            .execute('s_Gestao_Performance_Retorna_Dados_Colaborador')
+            .execute('s_Gestao_Performance_Retorna_Dados_Colaborador');
 
         let retorno = {
-            cargo: resultColaborador?.recordset
-        }
+            cargo: transformarFormato(resultColaborador?.recordset)
+        };
 
-        res.json(retorno)
-
+        res.json(retorno);
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json(error);
     }
-};
+});
 
-module.exports = router
+module.exports = router;
