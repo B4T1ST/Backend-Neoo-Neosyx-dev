eea950c25b0b04c6e145174dead07f93babacb7f
B4T1ST <ds.batista1994@hotmail.com>
2024-02-29 19:15:24 -0300

    ADD - funcao de dados colaborador

--- routes/gestaoPerformace/usuario/index.js
+++ routes/gestaoPerformace/usuario/index.js
@@ -1,57 +1,76 @@
-// Importando módulos
+//carregando modulos
 const sql = require('mssql');
 const express = require('express');
 const router = express.Router();
 const config = require('../../../config/config.json');
-const { get } = require('../../../lib/poolManager');
+const { get } = require('../../../lib/poolManager')
 const connection = require('../../../config/' + config.banco);
 
-// Função para transformar o formato do objeto
-function transformarFormato(objetoOriginal) {
-  const novoFormato = {
-    "cargo": []
-  };
-
-  for (const chave in objetoOriginal) {
-    if (objetoOriginal.hasOwnProperty(chave)) {
-      const novoArray = [];
-
-      for (const propriedade in objetoOriginal[chave]) {
-        if (objetoOriginal[chave].hasOwnProperty(propriedade)) {
-          const novoItem = {
-            label: propriedade,
-            value: objetoOriginal[chave][propriedade]
-          };
-
-          novoArray.push({ [propriedade]: novoItem });
-        }
-      }
-
-      novoFormato["cargo"].push(...novoArray);
-    }
-  }
-
-  return novoFormato;
-}
-
-// Rota
 router.get('/', async function (req, res) {
-    const { almope } = req.query;
-
     try {
-        let pool = await get('BDRechamadasGeral', connection);
+        const { almope } = req.query;
+
+        // Chama a função para obter os dados do colaborador
+        const resultColaborador = await retornaDadosUsuario(almope);
+
+        // Mapeia os resultados para o novo formato desejado
+        const mergedData = {
+            Nome: resultColaborador.nome,
+            Almope: resultColaborador.almope,
+            Avatar: resultColaborador.avatar,
+            IsSupervisor: resultColaborador.isSupervisor,
+            Cargo: resultColaborador.cargo,
+            Cliente: {
+                label: resultColaborador.cliente,
+                value: resultColaborador.idCliente,
+            },
+            Campanha: {
+                label: resultColaborador.campanha,
+                value: resultColaborador.idCampanha,
+            },
+            Diretor: {
+                label: resultColaborador.diretor,
+                value: resultColaborador.idDiretor,
+            },
+            Gerente: {
+                label: resultColaborador.gerente,
+                value: resultColaborador.idGerente,
+            },
+            Coordenador: {
+                label: resultColaborador.coordenador,
+                value: resultColaborador.idCoordenador,
+            },
+            Supervisor: {
+                label: resultColaborador.supervisor,
+                value: resultColaborador.idSupervisor,
+            },
+            Operador: {
+                label: resultColaborador.operador,
+                value: resultColaborador.idOperador,
+            },
+
+            // Adicione mais campos conforme necessário
+        };
+
+        res.json(mergedData);
+
+    } catch (error) {
+        console.error('Erro ao consultar o banco de dados:', error.message);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+});
+
+async function retornaDadosUsuario(almope) {
+    try {
+        let pool = await get('BDRechamadasGeral', connection)
         let resultColaborador = await pool.request()
             .input('almope', sql.VarChar, almope)
             .execute('s_Gestao_Performance_Retorna_Dados_Colaborador');
 
-        let retorno = {
-            cargo: transformarFormato(resultColaborador?.recordset)
-        };
-
-        res.json(retorno);
+        return resultColaborador.recordset[0]; // Assume que há apenas um registro retornado
     } catch (error) {
-        res.status(500).json(error);
+        throw error;
     }
-});
+}
 
 module.exports = router;
